9936641180323c10dc05a5ef709331894a08c076
VicentBG <vbatallerg@gmail.com>
2018-09-16 13:11:43 +0200

    Primer commit

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Vincent'}
+    posts = [
+        {
+            'author': {'username': 'Juanet'},
+            'body': 'Bonito dia por Cocen'
+        },
+        {
+            'author': {'username': 'Maruja'},
+            'body': 'La peli de los Vengadores es muy chula!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
